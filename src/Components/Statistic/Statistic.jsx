75ff21bd9efeb810a08a8d7019a27e9ba10c031f
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-09-27 12:10:08 +0600

    some error has been fixed! My piechart was not showing data as i wanted. SO fix this

--- src/Components/Statistic/Statistic.jsx
+++ src/Components/Statistic/Statistic.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import { getStoredDonationApplication } from "../../utility/localstorage";
 import { useLoaderData } from "react-router-dom";
-import donationGIF from "../../assets/images/donation.gif"
+import donationGIF from "../../assets/images/donation.gif";
 
 const Statistic = () => {
   const donations = useLoaderData();
@@ -17,20 +17,18 @@ const Statistic = () => {
     setTotalDonations(totalDonationCount);
     setYourDonations(yourDonationCount);
 
-   
-
     console.log(yourDonationCount, "and", totalDonationCount);
   }, [donations]);
 
-  const totalDonationPercentage = (totalDonations / (yourDonations + totalDonations)) * 100;
-  const yourDonationPercentage = 100 - totalDonationPercentage;
+  const yourDonationPercentage = (yourDonations / totalDonations) * 100;
+  const totalDonationPercentage = 100 - yourDonationPercentage;
 
   const data = [
     { name: "Your Donations", value: yourDonationPercentage },
     { name: "Total Donations", value: totalDonationPercentage },
   ];
 
-  const COLORS = ["#00C49F","#FF444A"]; // Your custom colors
+  const COLORS = ["#00C49F", "#FF444A"]; // Your custom colors
 
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
@@ -47,47 +45,47 @@ const Statistic = () => {
   console.log(yourDonationPercentage);
   return (
     <div className="flex flex-col items-center justify-center py-[50px]">
-      {
-        yourDonationPercentage === 0 ?(
-          <div className="flex flex-col items-center justify-center">
+      {yourDonationPercentage === 0 ? (
+        <div className="flex flex-col items-center justify-center">
           <img src={donationGIF} alt="" />
           <h1 className="text-4xl font-bold text-[#FF444A]">
             {`You haven't made any donations yet`}
           </h1>
         </div>
-        )
-        :
-        (
-    <div>
-      <div style={{ display: "flex", alignItems: "center", height: "60vh" , color:"white", fontWeight:"700", fontSize:"24px"}}>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="40%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={110}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
-    <div className="flex flex-col md:flex-row gap-4 lg:gap-[56px] mt-[0px] lg:mt-[-50px] px-[auto]">
-      <div className="flex gap-4 items-center justify-center text-[20px] font-semibold">Your Donation: {yourDonations} <div className="w-[80px] h-[10px] bg-[#00C49F] "></div></div>
-      <div className="flex gap-4 items-center justify-center text-[20px] font-semibold">Total Donation: {totalDonations} <div className="w-[80px] h-[10px] bg-[#FF444A] "></div></div>
-    </div>
+      ) : (
+        <div>
+          <div style={{ display: "flex", alignItems: "center", height: "60vh", color: "white", fontWeight: "700", fontSize: "24px" }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={data}
+                  cx="50%"
+                  cy="40%"
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  outerRadius={110}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
           </div>
-          
-        )
-      }
-    
+          <div className="flex flex-col md:flex-row gap-4 lg:gap-[56px] mt-[0px] lg:mt-[-50px] px-[auto]">
+            <div className="flex gap-4 items-center justify-center text-[20px] font-semibold">
+              Your Donation: {yourDonations}{" "}
+              <div className="w-[80px] h-[10px] bg-[#00C49F] "></div>
+            </div>
+            <div className="flex gap-4 items-center justify-center text-[20px] font-semibold">
+              Total Donation: {totalDonations}{" "}
+              <div className="w-[80px] h-[10px] bg-[#FF444A] "></div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
